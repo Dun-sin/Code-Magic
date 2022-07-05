3ae27ba2b41ce1cb7b899379344f08847ce4c215
Dunsin <jesudunsinfaiye@gmail.com>
2022-07-05 12:59:08 +0100

    feat: add min height to image preview

--- src/main.ts
+++ src/main.ts
@@ -8,34 +8,45 @@ import FilePondPluginImageTransform from 'filepond-plugin-image-transform';
 
 import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css';
 
-let attributeValue: string | null;
-
-const generators = document.querySelectorAll('[data-gen]');
-// const countElement = document.querySelector('.count > span');
-const textInput = <HTMLInputElement>(
-	document.getElementById(`${'pic-text'}-text`)
-);
-const buttonElement = <HTMLInputElement>(
-	document.querySelector(`[data-button = ${'pic-text'}]`)
-);
-
-// File Pond
-const imageElement = <HTMLInputElement>(
-	document.getElementById(`${'pic-text'}-file`)
-);
-
 FilePond.registerPlugin(
 	FilePondPluginImagePreview,
 	FilePondPluginImageResize,
 	FilePondPluginImageTransform,
 );
 
-const pond = FilePond.create(imageElement, {
+let attributeValue: string | null;
+
+// Elements
+const generators = document.querySelectorAll('[data-gen]');
+// const countElement = document.querySelector('.count > span');
+const textInputElement = <HTMLInputElement>(
+	document.getElementById(`${'pic-text'}-text`)
+);
+const getImageButtonElement = <HTMLInputElement>(
+	document.querySelector(`[data-button = ${'pic-text'}]`)
+);
+
+// File Pond Element & Options
+let imageSRC: string;
+const imageEntryElement = <HTMLInputElement>(
+	document.getElementById(`${'pic-text'}-file`)
+);
+
+FilePond.create(imageEntryElement, {
 	imagePreviewMinHeight: 400,
+
+	onpreparefile: (fileItem, output) => {
+		// create a new image object
+		const img = new Image();
+
+		// set the image source to the output of the Image Transform plugin
+		img.src = URL.createObjectURL(output);
+		imageSRC = img.src;
+	},
 });
 
-buttonElement.addEventListener('click', () => {
-	console.log(textInput?.value);
+getImageButtonElement.addEventListener('click', () => {
+	console.log(textInputElement?.value);
 });
 
 generators.forEach((generator) => {
