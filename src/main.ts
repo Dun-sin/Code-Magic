9f8cad52bca63032203733ee6c4903bdaf645db5
Dunsin <jesudunsinfaiye@gmail.com>
2022-07-08 00:04:30 +0100

    feat: make onclick on `generator` work by generator name

--- src/main.ts
+++ src/main.ts
@@ -14,21 +14,27 @@ FilePond.registerPlugin(
 	FilePondPluginImageTransform,
 );
 
-let attributeValue: string | null;
+let attributeValue: string | null = null;
 let isOpen: boolean;
+let imageText = `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae placeat odit porro sint illo deserunt totam reiciendis voluptatibus qui dolores. Veritatis beatae eius minima aliquid blanditiis earum delectus itaque natus?Quisquam nihil hic tempora reprehenderit eum quam, illo voluptatem ex tenetur, cumque accusamus ipsum ad necessitatibus nobis, fuga voluptatum mollitia. Provident quas nam vero in, officiis explicabo est neque optio!Voluptas aperiam libero excepturi saepe praesentium! Quasi provident sapiente saepe, harum voluptate voluptatem incidunt animi nesciunt quis nam exercitationem at maiores dignissimos hic consequatur, et accusantium tempore natus enim fugiat.Aspernatur id atque accusantium ad delectus, praesentium minima voluptas iure quos repellat quo quaerat doloribus libero expedita, nisi ex nobis earum.
+	   `;
 
 // Elements
 const generators = document.querySelectorAll('[data-gen]');
+
+generators.forEach((generator) => {
+	generator?.addEventListener('click', () => {
+		isOpen = true;
+		isVisible(isOpen);
+		const checking = generator.getAttribute('data-gen');
+		if (checking === null) return;
+
+		attributeValue = checking;
+		checkingIfGeneratorExists(attributeValue);
+	});
+});
 // const countElement = document.querySelector('.count > span');
-const getTextInputElement = <HTMLInputElement>(
-	document.getElementById(`${'pic-text'}-text`)
-);
-const getImageButtonElement = <HTMLInputElement>(
-	document.querySelector(`[data-button = ${'pic-text'}]`)
-);
-const getOutputElement: HTMLElement | null = document.querySelector(
-	`[data-model=${'pic-text'}]  .output`,
-);
+
 const closeModalElement = document.getElementById('close-modal');
 const modalContainerElement = <HTMLElement>(
 	document.querySelector('.modal-container')
@@ -36,49 +42,50 @@ const modalContainerElement = <HTMLElement>(
 
 // File Pond Element & Options
 let imageSRC: string;
-const getImageEntryElement = <HTMLInputElement>(
-	document.getElementById(`${'pic-text'}-file`)
-);
 
-FilePond.create(getImageEntryElement, {
-	imagePreviewMaxHeight: 200,
+function checkingIfGeneratorExists(attribute: string | null) {
+	if (attribute === null) return;
 
-	onpreparefile: (fileItem, output) => {
-		// create a new image object
-		const img = new Image();
+	const getTextInputElement = <HTMLInputElement>(
+		document.getElementById(`${attributeValue}-text`)
+	);
+	const getImageButtonElement = <HTMLInputElement>(
+		document.querySelector(`[data-button = ${attributeValue}]`)
+	);
+	const getOutputElement: HTMLElement | null = document.querySelector(
+		`[data-model=${attributeValue}]  .output`,
+	);
 
-		// set the image source to the output of the Image Transform plugin
-		img.src = URL.createObjectURL(output);
-		imageSRC = img.src;
-	},
-});
-
-getImageButtonElement.addEventListener('click', () => {
-	if (getOutputElement === null) {
-		return;
-	}
-	getOutputElement.style.background = `url(${imageSRC}) center no-repeat`;
-	getOutputElement.style.backgroundSize = '300px';
-	getOutputElement.style.backgroundClip = 'text';
-	getOutputElement.style.webkitBackgroundClip = 'text';
-	getOutputElement.style.webkitTextFillColor = 'rgba(255, 255, 255, 0.1)';
-	getOutputElement.innerText = `Lorem ipsum dolor, sit amet consectetur adipisicing elit. Repudiandae placeat odit porro sint illo deserunt totam reiciendis voluptatibus qui dolores. Veritatis beatae eius minima aliquid blanditiis earum delectus itaque natus?Quisquam nihil hic tempora reprehenderit eum quam, illo voluptatem ex tenetur, cumque accusamus ipsum ad necessitatibus nobis, fuga voluptatum mollitia. Provident quas nam vero in, officiis explicabo est neque optio!Voluptas aperiam libero excepturi saepe praesentium! Quasi provident sapiente saepe, harum voluptate voluptatem incidunt animi nesciunt quis nam exercitationem at maiores dignissimos hic consequatur, et accusantium tempore natus enim fugiat.Aspernatur id atque accusantium ad delectus, praesentium minima voluptas iure quos repellat quo quaerat doloribus libero expedita, nisi ex nobis earum.
-	   `;
-	console.log(getTextInputElement?.value);
-});
-
-generators.forEach((generator) => {
-	generator?.addEventListener('click', () => {
-		isOpen = true;
-		isVisible(isOpen);
-		attributeValue = generator.getAttribute('data-gen');
-		if (attributeValue === null) {
+	getImageButtonElement.addEventListener('click', () => {
+		if (getOutputElement === null) {
 			return;
-		} else {
-			console.log(attributeValue);
 		}
+		getOutputElement.style.background = `url(${imageSRC}) center no-repeat`;
+		getOutputElement.style.backgroundSize = '400px';
+		getOutputElement.style.backgroundClip = 'text';
+		getOutputElement.style.webkitBackgroundClip = 'text';
+		getOutputElement.style.webkitTextFillColor = 'rgba(255, 255, 255, 0.1)';
+		getOutputElement.innerText = imageText;
+		console.log(getTextInputElement?.value);
 	});
-});
+
+	const getImageEntryElement = <HTMLInputElement>(
+		document.getElementById(`${attributeValue}-file`)
+	);
+
+	FilePond.create(getImageEntryElement, {
+		imagePreviewMaxHeight: 200,
+
+		onpreparefile: (fileItem, output) => {
+			// create a new image object
+			const img = new Image();
+
+			// set the image source to the output of the Image Transform plugin
+			img.src = URL.createObjectURL(output);
+			imageSRC = img.src;
+		},
+	});
+}
 
 closeModalElement?.addEventListener('click', () => {
 	isOpen = false;
