3771205b4ba4b105f6dcf32098de9f04f2126803
Pushkaraj Kulkarni <pushkarajkulkarni07@gmail.com>
2022-08-12 23:30:38 +0530

    feat: added popup that code has been copied
    Added a apopup that code has been copied to clipboard when user presses the Get Code but on any type
    of generatorgeneratorton
    
    fix #91

--- src/generators/gradient-background.ts
+++ src/generators/gradient-background.ts
@@ -40,5 +40,6 @@ function getGradientBackgroundResult(
 	const getCodeButtonElement = utils.getCopyCodeButton(attribute);
 	getCodeButtonElement.addEventListener('click', () => {
 		utils.copyCodeToClipboard(attribute, outputElement);
+		utils.showPopup('Code Copied', 'Code has been successfully copied to clipboard', 'success')
 	});
 }
