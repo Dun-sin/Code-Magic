8e230e0fbe9f8c6fc138d91a7f5256c03273b67b
Dunsin <jesudunsinfaiye@gmail.com>
2022-09-18 16:56:38 +0100

    feat: rebrand the visual

--- src/pages/pic-text.ts
+++ src/pages/pic-text.ts
@@ -1,40 +1,25 @@
 import * as utils from '../lib/general';
 
-import * as FilePond from 'filepond';
-import 'filepond/dist/filepond.min.css';
-
-// File Pond Plugins
-import FilePondPluginImagePreview from 'filepond-plugin-image-preview';
-import FilePondPluginImageResize from 'filepond-plugin-image-resize';
-import FilePondPluginImageTransform from 'filepond-plugin-image-transform';
-
-import 'filepond-plugin-image-preview/dist/filepond-plugin-image-preview.css';
-
-FilePond.registerPlugin(
-  FilePondPluginImagePreview,
-  FilePondPluginImageResize,
-  FilePondPluginImageTransform
-);
-
-let imageSRC: string;
-
 /**
  * @function picTextGenerator
  * @summary function for all the functions of the pic-text generator
  * @param attribute - The attribute name of the generator element
  * @return {void} Nothing
  */
-export function picTextGenerator(): void {
+export function picTextGenerator(image: string): void {
   const attribute = 'pic-text';
-  getImageFile(attribute);
-  const getTextInputElement = <HTMLInputElement>(
-    document.getElementById(`${attribute}-text`)
-  );
+  const outputNode = utils.getOutput(attribute);
 
-  const getOutputElement = utils.getOutput(attribute);
+  let imageText = `################################################################################################################################################################################################################################################################################################################################################################################################################################################################################################################################################################################################`;
 
-  utils.countForText(getTextInputElement);
-  getPicTextResult(attribute, getOutputElement, getTextInputElement.value);
+  outputNode.style.background = `url(${image}) center no-repeat`;
+  outputNode.style.width = 'var(--output-width)';
+  outputNode.style.backgroundSize = 'var(--output-width)';
+  outputNode.style.backgroundClip = 'text';
+  outputNode.style.webkitBackgroundClip = 'text';
+  outputNode.style.webkitTextFillColor = 'rgba(255, 255, 255, 0.1)';
+  outputNode.innerText = imageText;
+  getPicTextResult(attribute, outputNode);
 }
 
 /**
@@ -45,16 +30,7 @@ export function picTextGenerator(): void {
  * @param text - The text to display on the image
  * @return {void} nothing
  */
-function getPicTextResult(
-  attribute: string,
-  outputNode: HTMLElement,
-  text: string
-): void {
-  let imageText = `################################################################################################################################################################################################################################################################################################################################################################################################################################################################################################################################################################################################`;
-
-  const getImageButtonElement = <HTMLInputElement>(
-    document.querySelector(`[data-button = ${attribute}]`)
-  );
+function getPicTextResult(attribute: string, outputNode: HTMLElement): void {
   const getCodeButtonElement = utils.getCopyCodeButton(attribute);
   const getPNGButtonElement = utils.getPNGButton(attribute);
   const getSVGButtonElement = utils.getSVGButton(attribute);
@@ -63,9 +39,6 @@ function getPicTextResult(
     return;
   }
 
-  getImageButtonElement.addEventListener('click', (): void => {
-    setOnClick();
-  });
   getPNGButtonElement.addEventListener('click', () => {
     utils.downloadPNG(attribute, outputNode);
   });
@@ -79,43 +52,5 @@ function getPicTextResult(
       'Code has been successfully copied to clipboard',
       'success'
     );
-  });
-
-  function setOnClick() {
-    outputNode.style.background = `url(${imageSRC}) center no-repeat`;
-    outputNode.style.backgroundSize = 'var(--output-width)';
-    outputNode.style.backgroundClip = 'text';
-    outputNode.style.webkitBackgroundClip = 'text';
-    outputNode.style.webkitTextFillColor = 'rgba(255, 255, 255, 0.1)';
-    if (text !== '') {
-      imageText = text;
-    }
-    outputNode.innerText = imageText;
-  }
-}
-
-/**
- * @function getImageFile
- * @summary Gets the image file and set the image to the imageSRC
- * @param attribute {string} The attribute name of the generator element
- * @return {void} nothing
- */
-export function getImageFile(attribute: string): void {
-  const getImageEntryElement = <HTMLInputElement>(
-    document.getElementById(`${attribute}-file`)
-  );
-
-  FilePond.create(getImageEntryElement, {
-    imagePreviewMaxHeight: 200,
-
-    onpreparefile: (fileItem, output): void => {
-      // create a new image object
-      const img = new Image();
-
-      // set the image source to the output of the Image Transform plugin
-      img.src = URL.createObjectURL(output);
-      imageSRC = img.src;
-      console.log(fileItem);
-    },
   });
 }
