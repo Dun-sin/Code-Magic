83b2291c30fdfc75b1b5a0870d8626cd71312c22
Cristian Toffanin <toffanin.c@gmail.com>
2022-10-11 07:03:07 +0200

    Add rotation animation

--- src/pages/animation.ts
+++ src/pages/animation.ts
@@ -138,6 +138,29 @@ function manageAnimation(
     OutputElement.style.animationDuration = `${values.duration}s`;
 
     // rule_added = true;
+  } else if (values.type === 'rotate') {
+    console.log(values);
+
+    css =
+      `/*Copy and paste keyframe into your css file, and apply the animation property in the element of your choice*/\n` +
+      `@keyframes rotate { \n` +
+      `0%   { transform:rotate(0deg); }\n` +
+      `100% { transform:rotate(${values.degree}deg); }\n` +
+      `}\n` +
+      `animate: rotate ${values.duration}s infinite`;
+
+    stylesheet.insertRule(
+      `@keyframes rotate { \n` +
+        `0%   { transform:rotate(0deg); }\n` +
+        `100% { transform:rotate(${values.degree}deg); }\n` +
+        `}`,
+      initial_length + 1
+    );
+
+    OutputElement.style.animation = `rotate ease-in`;
+    OutputElement.style.animationDuration = `${values.duration}s`;
+
+    // rule_added = true;
   }
 }
 
@@ -180,6 +203,13 @@ function initialConfiguration(
         DegreeElement.value = '50';
         unitDisplayElement.innerText = `${DegreeElement.value}deg`;
         titleDisplayElement.innerText = 'Angle';
+      } else if (type === 'rotate') {
+        DegreeElement.min = '0';
+        DegreeElement.max = '360';
+        DegreeElement.step = '1';
+        DegreeElement.value = '45';
+        unitDisplayElement.innerText = `${DegreeElement.value}deg`;
+        titleDisplayElement.innerText = 'Degrees';
       } else {
         DegreeElement.min = '0';
         DegreeElement.max = '1';
