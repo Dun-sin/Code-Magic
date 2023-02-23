b958dbb44ebed1bd22af042a5259236826e133c1
Dunsin <78784850+Dun-sin@users.noreply.github.com>
2023-02-23 21:52:27 +0100

    Update src/pages/border-radius.ts

--- src/pages/border-radius.ts
+++ src/pages/border-radius.ts
@@ -62,7 +62,7 @@ export function borderRadiusGenerator(
   if (type === null) return;
 
   const getOutputElement = getOutput(attribute);
-  const resultPage = getResultPage(); //page to hold result
+  const resultPage = getResultPage();
 
   resultPage.style.display = 'flex';
   if (getOutputElement === null || type === 'oldResults') return;
