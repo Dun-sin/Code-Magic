c99255bffde19839371120cba1ea7a66f61e6cb9
Saket Kaswa <kaswasd21.mfg@coep.ac.in>
2023-10-01 18:33:18 +0530

    fix(main.ts): fixed the drop-down issue
    Now the drop down open or closes even if we click in the blank space
    
    #468

--- src/main.ts
+++ src/main.ts
@@ -434,10 +434,7 @@ getRadioButtonSetElement.forEach((radioButton: HTMLInputElement) => {
 dropDownElements.forEach((dropDown) => {
   // add click event listener to the dropdown parent element
   dropDown.addEventListener('click', (e) => {
-    // keep dropdown open when a subitem is clicked on
     e.stopPropagation();
-    const target = e.target as HTMLElement;
-    if (!dropDown.children[0].innerHTML.includes(target.outerHTML)) return;
 
     const listElement = dropDown.lastElementChild as HTMLElement;
     if (listElement.id === 'showList') {
